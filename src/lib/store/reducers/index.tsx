0bb7e69d64610546f2821beaa6d02f8c15ce056b
Alex Bandisch <alexander.bandisch@gmail.com>
2018-11-02 12:45:32 +1000

    rm actions and reducers

--- src/lib/store/reducers/index.tsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import mockData from './mockData'
-
-export default combineReducers({
-  mockData
-})
--- src/lib/types/index.tsx
+++ src/lib/types/index.tsx
@@ -1,5 +1,3 @@
-import * as actionTypes from '../store/actions/actionTypes'
-
 export interface IRootState {
   mockData: {
     data: string,
@@ -9,19 +7,3 @@ export interface IRootState {
     }
   }
 }
-
-export interface IGetMockDataActionRequest {
-  type: actionTypes.GET_MOCK_DATA_REQUEST
-}
-
-export interface IGetMockDataActionSuccess {
-  type: actionTypes.GET_MOCK_DATA_SUCCESS,
-  data: string
-}
-
-export interface IGetMockDataActionError {
-  type: actionTypes.GET_MOCK_DATA_ERROR,
-  message: string
-}
-
-export type MockDataActions = IGetMockDataActionRequest | IGetMockDataActionSuccess | IGetMockDataActionError
